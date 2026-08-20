158d20759e598ba1f34b9dec163f1f9eb6b57f44
VincentOUALLET <vincent.ouallet@yahoo.fr>
2026-08-20 16:39:50 +0200

    fix: use bootstrap JS registration instead of Util::addScript

--- lib/Bootstrap/Application.php
+++ lib/Bootstrap/Application.php
@@ -4,12 +4,10 @@ namespace OCA\Renamer\Bootstrap;
 
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use OCP\AppFramework\Bootstrap\IJSRooter;
-use OCP\Util;
 
 class Application implements IBootstrap {
     public function register(IRegistrationContext $context): void {
-        Util::addScript('renamer', 'rename');
+        $context->registerJSScript('renamer', 'rename');
     }
 
     public function boot(): void {
