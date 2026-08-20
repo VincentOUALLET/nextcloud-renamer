57f282fb2dc506a5dff7f399c1dcc21323e08a12
VincentOUALLET <vincent.ouallet@yahoo.fr>
2026-08-20 17:58:40 +0200

    debug: bootstrap tracing and rename.js debug logs

--- lib/Bootstrap/Application.php
+++ lib/Bootstrap/Application.php
@@ -1,5 +1,5 @@
 <?php
-error_log('Renamer bootstrap file loaded');
+error_log('[Renamer][Bootstrap] file loaded');
 
 namespace OCA\Renamer\Bootstrap;
 
@@ -7,44 +7,44 @@ use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
 use OCP\Util;
 
-error_log('Renamer bootstrap namespace loaded');
+error_log('[Renamer][Bootstrap] namespace loaded');
 
-if (!interface_exists(\OCP\AppFramework\Bootstrap\IBootstrap::class)) {
-    error_log('Renamer bootstrap: IBootstrap interface NOT FOUND');
+if (!interface_exists(IBootstrap::class)) {
+    error_log('[Renamer][Bootstrap] IBootstrap interface NOT FOUND');
 } else {
-    error_log('Renamer bootstrap: IBootstrap interface FOUND');
+    error_log('[Renamer][Bootstrap] IBootstrap interface FOUND');
 }
 
 class Application implements IBootstrap {
     public function __construct() {
-        error_log('Renamer bootstrap Application instantiated');
+        error_log('[Renamer][Bootstrap] instantiated');
     }
 
     public function register(IRegistrationContext $context): void {
-        error_log('Renamer bootstrap register() called');
+        error_log('[Renamer][Bootstrap] register() called');
 
         if (method_exists($context, 'registerJSScript')) {
             try {
                 $context->registerJSScript('renamer', 'rename');
-                error_log('Renamer bootstrap registerJSScript ok');
+                error_log('[Renamer][Bootstrap] registerJSScript ok');
             } catch (\Throwable $e) {
-                error_log('Renamer bootstrap registerJSScript failed: ' . $e->getMessage());
+                error_log('[Renamer][Bootstrap] registerJSScript failed: ' . $e->getMessage());
             }
         } else {
-            error_log('Renamer bootstrap: registerJSScript NOT available on context');
+            error_log('[Renamer][Bootstrap] registerJSScript NOT available');
         }
 
         try {
             Util::addScript('renamer', 'rename');
-            error_log('Renamer bootstrap Util::addScript ok');
+            error_log('[Renamer][Bootstrap] addScript ok');
         } catch (\Throwable $e) {
-            error_log('Renamer bootstrap addScript failed: ' . $e->getMessage());
+            error_log('[Renamer][Bootstrap] addScript failed: ' . $e->getMessage());
         }
     }
 
     public function boot(): void {
-        error_log('Renamer bootstrap boot() called');
+        error_log('[Renamer][Bootstrap] boot() called');
     }
 }
 
-error_log('Renamer bootstrap class defined');
+error_log('[Renamer][Bootstrap] class defined');
