dc25f6ca5c8120a30785d17f432f5d2f6298b2fc
Vincent OUALLET <vincent.ouallet@yahoo.fr>
2026-08-26 11:18:01 +0000

    fix: restore file action registration in rename.js loader

--- js/rename.js
+++ js/rename.js
@@ -26,9 +26,33 @@
         document.head.appendChild(link);
     }
 
+    function openWhenReady() {
+        if (typeof RenamerApp !== 'undefined' && RenamerApp.openDialog) {
+            try { RenamerApp.openDialog(); } catch (e) { log('openDialog failed', e); }
+        } else {
+            setTimeout(openWhenReady, 50);
+        }
+    }
+
+    function registerAction() {
+        window._nc_fileactions = window._nc_fileactions || [];
+        if (!window._nc_fileactions.some(function(a) { return a && a.id === 'rename-auto'; })) {
+            window._nc_fileactions.push({
+                id: 'rename-auto',
+                displayName: 'Rename Auto',
+                mimeType: 'all',
+                permissions: (typeof OC !== 'undefined' && OC.PERMISSION_UPDATE) ? OC.PERMISSION_UPDATE : 16,
+                actionHandler: function() { openWhenReady(); },
+                order: 100
+            });
+            log('registered file action into window._nc_fileactions (count=' + window._nc_fileactions.length + ')');
+        }
+    }
+
     function init() {
         log('loaded');
         injectStyle(OC.generateUrl('/apps/renamer/css/style'), 'renamer-css').catch(function(){});
+        registerAction();
         injectScript(OC.generateUrl('/apps/renamer/js/utils'), 'renamer-utils')
             .then(function() { return injectScript(OC.generateUrl('/apps/renamer/js/app'), 'renamer-app'); })
             .catch(function(err) {
